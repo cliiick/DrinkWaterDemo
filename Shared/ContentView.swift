23007caa0f915c34254fec2350d1a382b2bfb7ba
DesignBall <15620690068@163.com>
2021-07-09 14:59:21 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by dongshihua on 2021/7/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/DrinkWaterDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkWaterDemoApp.swift
+//  Shared
+//
+//  Created by dongshihua on 2021/7/9.
+//
+
+import SwiftUI
+
+@main
+struct DrinkWaterDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

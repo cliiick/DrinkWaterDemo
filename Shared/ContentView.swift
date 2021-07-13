2d46014e69225278ead2b6cf8afc45821ce0257c
DesignBall <15620690068@163.com>
2021-07-13 11:01:05 +0800

    增加了喝水判断的界面和条件

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isWatered = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ZStack {
+                Image("drinkImg")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                Image(systemName: isWatered ? "crown.fill" : "eyes.inverse")
+                    .foregroundColor(isWatered ? .yellow : .white)
+                    .font(.system(size: 128))
+            }
+            .frame(minHeight:640)
+            .shadow(color: .blue.opacity(0.3), radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: 4, y: 0.0)
+            
+            Divider()
+            
+            Toggle(isOn: $isWatered) {
+                Label("今天你喝水了吗？", systemImage: "drop.fill")
+            }.foregroundColor(.blue)
+        }
+        .padding()
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
